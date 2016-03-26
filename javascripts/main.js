8cb40c60d8509b20c876a096c0f91e4713892597
Wali Usmani <wusmani@cs.ubc.ca>
2016-03-26 13:03:15 -0700

    First view

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+   $(".nav-pills li").click(function(){
+       $(".active").removeClass("active");
+       $(this).addClass("active");
+   });
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
